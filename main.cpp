2843177272b610b3a3813c96309708373fbdabac
pd0220 <david.pesznyak@gmail.com>
2020-03-05 01:16:52 +0100

    finish

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@
 #include <math.h>
 #include <string>
 #include <vector>
+#include <numeric>
+
+// -----------------------------------------------------------------------------------------------------------------------------------
 
 // read given file
 std::vector<double> readFile(std::string fileName)
@@ -43,7 +46,41 @@ std::vector<double> readFile(std::string fileName)
     return dataStructure;
 }
 
-//
+// -----------------------------------------------------------------------------------------------------------------------------------
+
+// calculate estimator from given set of data (over the whole set)... simply mean
+double mean(std::vector<double> rawData)
+{
+    // mean
+    return std::accumulate(rawData.begin(), rawData.end(), 0.) / static_cast<double>(rawData.size());
+}
+
+// -----------------------------------------------------------------------------------------------------------------------------------
+
+// calculate variance for jackknife method
+double variance(std::vector<double> subsetMeans, double N, double estimator)
+{
+    // calculate pre-factor
+    double preFactor = (N - 1) / N;
+    // calculate sum
+    auto add_square = [estimator](double sum, int i) {auto d = i - estimator; return sum + d * d; };
+    return preFactor * std::accumulate(subsetMeans.begin(), subsetMeans.end(), 0.0, add_square);
+}
+
+// -----------------------------------------------------------------------------------------------------------------------------------
+
+// taking out the nth value from the full set and returning the subset
+std::vector<double> jackknifeCut(std::vector<double> rawData, int n)
+{
+    // copy the whole set
+    std::vector subSet = rawData;
+    // erase nth value from copy
+    subSet.erase(subSet.begin() + n);
+    // return subset
+    return subSet;
+}
+
+// -----------------------------------------------------------------------------------------------------------------------------------
 
 // main function
 // argv[1] is the datafile to analyze
@@ -51,6 +88,7 @@ int main(int argc, char **argv)
 {
     // file name
     std::string fileName = "None";
+
     // check for arguments
     if (argc > 1)
     {
@@ -65,6 +103,47 @@ int main(int argc, char **argv)
 
     // create container for raw data
     std::vector<double> rawData = readFile(fileName);
+    // size of vector
+    double N = static_cast<double>(rawData.size());
+
+    // calculate estimator from full set of data
+    double estimator = mean(rawData);
+
+    // create N subsets always taking out the nth value and calculate means
+    // empty vector
+    std::vector<double> subsetMeans(0, 0);
+
+    double test = 0;
+
+    // jackknife subsets
+    for (int i = 0; i < N; i++)
+    {
+        subsetMeans.push_back(mean(jackknifeCut(rawData, i)));
+        test += (subsetMeans[i] - estimator) * (subsetMeans[i] - estimator);
+    }
+
+    // calculete variance
+    double var = variance(subsetMeans, N, estimator);
+
+    // check if bias to calculated or not
+    if (argc > 2)
+    {
+        std::string ifBias = argv[2];
+        if (ifBias == "unbiased")
+        {
+            // calculate bias
+            double bias = mean(subsetMeans);
+
+            // calculate unbiased estimator
+            double estimatorUnbiased = estimator - (N - 1) * (bias - estimator);
+
+            std::cout << "The unbiased estimator is: " << estimatorUnbiased << " with s.d.: " << std::sqrt(var) << std::endl;
+        }
+    }
+    else
+    {
+        std::cout << "The estimator is: " << estimator << " with s.d.: " << std::sqrt(var) << std::endl;
+    }
 
     return 0;
 }
